bb8d2f6e659ef3b1b8d489a55193028ddf391a28
Star_sider <dxsider@163.com>
2017-01-04 17:28:21 +0800

    日常提交

--- LaneWeChat/yangjiguanjia/send_happyegg/assets/js/commom.js
+++ LaneWeChat/yangjiguanjia/send_happyegg/assets/js/commom.js
@@ -1,6 +1,6 @@
 const validate = {
 			//域名
-			url:"https://test.wangbaojin1006.cn",
+			url:"http://weixin.yangjiguanjia.com",
 		    //phone
 		    phone: function(phone){
 		        if(!/^(13[0-9]|15[0|1|2|3|6|7|8|9]|18[0-9]|177)\d{8}$/.test(phone)){
