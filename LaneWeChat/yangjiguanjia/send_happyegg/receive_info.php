8bcbb00d4b77cfbcd05d17e20fed7aefb4a4e8d3
Star_sider <dxsider@163.com>
2017-01-06 13:41:21 +0800

    日常提交

--- LaneWeChat/yangjiguanjia/send_happyegg/receive_info.php
+++ LaneWeChat/yangjiguanjia/send_happyegg/receive_info.php
@@ -15,208 +15,190 @@
 			#example-1 li{display: flex;width: 100%;}
 			#example-1 span{flex: 1;text-align: center;}
 		
-        .container{
-        	z-index: 999;
-            text-align: center;
-            width: 100%;
-            /*max-width: 960px;*/
-            margin: 0 auto;
-            padding: 0 5px;
-            background-color: rgba(0,0,0,0.3);
-            height: 100%;
-            position: absolute;
-            top: 0;
-        }
-        .hongbao{
-            height: 9rem;
-            background: #A5423A;
-            width: 6rem;
-            left: 0;
-            top: 0;
-            border-radius: 10px;
-            margin: .2rem auto 0;
-        }
-        .topcontent{
-            height: 280px;
-            border: 1px solid #BD503A;
-            background-color: #BD503A;
-            border-radius: 10px 10px 50% 50% / 10px 10px 15% 15%;
-            box-shadow: 0px 4px 0px -1px rgba(0,0,0,0.2);
-        }
-        .avatar{
-            position: relative;
-        }
-        .avatar span{
-            position: absolute;
-            top: 10px;
-            right: 15px;
-            -webkit-transform: rotate(45deg);
-                -ms-transform: rotate(45deg);
-                    transform: rotate(45deg);
-            font-size: 2em;
-            font-weight: bolder;
-        }
-        #close{
-            color: rgba(0,0,0,0.5);
-        }
-        .avatar img{
-            border: 1px solid #BD503A;
-            border-radius: 50%;
-            overflow: hidden;
-            margin-top: 10%;
-        }
-        .topcontent h2{
-            margin: 15px 0;
-        }
-        .text{
-            color: #999;
-        }
-        .description{
-            margin: 15px 0;
-            font-size: .16rem;
-            font-weight: 600;
-        }
-        #chai{
-        	
-            width: 2rem;
-            height: 2rem;
-            border: 1px solid #FFA73A;
-            background-color: #FFA73A;
-            border-radius: 50%;
-            color: #fff;
-            font-size: .2rem;
-            display: inline-block;
-            margin-top: -50px;
-            box-shadow: 0px 4px 0px 0px rgba(0, 0, 0, 0.2);
-            font-size: 26px;font-weight: 600;line-height: 100px;
-        }
-        #chai span{
-            margin-top: 35px;
-            display: inline-block;
-            line-height: 70px;
-        }
-        .rotate{
-            -webkit-animation: anim .6s alternate;
-                -ms-animation: anim .6s alternate;
-                    animation: anim .6s alternate;
-        }
-        @-webkit-keyframes anim {
-            from { -webkit-transform: rotateY(0deg); }
-            to { -webkit-transform: rotateY(360deg); }
-        }
-        @-ms-keyframes anim {
-            from { -ms-transform: rotateY(0deg); }
-            to { -ms-transform: rotateY(360deg); }
-        }
-        @keyframes anim {
-            from { transform: rotateY(0deg); }
-            to { transform: rotateY(360deg); }
-        }
+	        .container{
+	        	z-index: 999;
+	            text-align: center;
+	            width: 100%;
+	            /*max-width: 960px;*/
+	            margin: 0 auto;
+	            padding: 0 5px;
+	           
+	            height: 100%;
+	            position: absolute;
+	            top: 0;
+	        }
+		    #hongbao{
+		        height: 9rem;
+		        background: #A5423A;
+		        width: 6rem;
+		        left: 0;
+		        top: 0;
+		        border-radius: 10px;
+		        margin: .2rem auto 0;
+		 
+		        -webkit-animation-delay: 0.6s;
+		        -moz-animation-delay: 0.6s;
+		        -o-animation-delay: 0.6s;
+		        animation-delay: 0.6s;
+		    }
+	        .topcontent{
+	            height: 280px;
+	            border: 1px solid #BD503A;
+	            background-color: #BD503A;
+	            border-radius: 10px 10px 50% 50% / 10px 10px 15% 15%;
+	            box-shadow: 0px 4px 0px -1px rgba(0,0,0,0.2);
+	        }
+	        .avatar{
+	            position: relative;
+	        }
+	        .avatar span{
+	            position: absolute;
+	            top: 10px;
+	            right: 15px;
+	            -webkit-transform: rotate(45deg);
+	                -ms-transform: rotate(45deg);
+	                    transform: rotate(45deg);
+	            font-size: 2em;
+	            font-weight: bolder;
+	        }
+	        #close{
+	            color: rgba(0,0,0,0.5);
+	        }
+	        .avatar img{
+	            border: 1px solid #BD503A;
+	            border-radius: 50%;
+	            overflow: hidden;
+	            margin-top: 10%;
+	        }
+	        .topcontent h2{
+	            margin: 15px 0;
+	        }
+	        .text{
+	            color: #999;
+	        }
+	        .description{
+	            margin: 15px 0;
+	            font-size: .16rem;
+	            font-weight: 600;
+	        }
+	        #chai{
+	        	
+	            width: 2rem;
+	            height: 2rem;
+	            border: 1px solid #FFA73A;
+	            background-color: #FFA73A;
+	            border-radius: 50%;
+	            color: #fff;
+	            font-size: .2rem;
+	            display: inline-block;
+	            margin-top: -50px;
+	            box-shadow: 0px 4px 0px 0px rgba(0, 0, 0, 0.2);
+	            font-size: 26px;font-weight: 600;line-height: 100px;
+	        }
+	        #chai span{
+	            margin-top: 35px;
+	            display: inline-block;
+	            line-height: 70px;
+	        }
+	        .rotate{
+	            -webkit-animation: anim .6s alternate;
+	                -ms-animation: anim .6s alternate;
+	                    animation: anim .6s alternate;
+	        }
+	        
+	        #got{
+	        	 -webkit-animation-delay: 1s;
+		        -moz-animation-delay: 1s;
+		        -o-animation-delay: 1s;
+		        animation-delay: 1s;
+	        }
+	        @-webkit-keyframes anim {
+	            from { -webkit-transform: rotateY(0deg); }
+	            to { -webkit-transform: rotateY(360deg); }
+	        }
+	        @-ms-keyframes anim {
+	            from { -ms-transform: rotateY(0deg); }
+	            to { -ms-transform: rotateY(360deg); }
+	        }
+	        @keyframes anim {
+	            from { transform: rotateY(0deg); }
+	            to { transform: rotateY(360deg); }
+	        }
+	        
+	        
+	        .bounce-enter-active {
+			  animation: bounce-in .5s;
+			}
+			.bounce-leave-active {
+			  animation: bounce-out .5s;
+			}
+			@keyframes bounce-in {
+			  0% {
+			    transform: scale(0);
+			  }
+			  50% {
+			    transform: scale(1.5);
+			  }
+			  100% {
+			    transform: scale(1);
+			  }
+			}
+			@keyframes bounce-out {
+			  0% {
+			    transform: scale(1);
+			  }
+			  50% {
+			    transform: scale(1.5);
+			  }
+			  100% {
+			    transform: scale(0);
+			  }
+			}
         
-        
-        .bounce-enter-active {
-		  animation: bounce-in .5s;
-		}
-		.bounce-leave-active {
-		  animation: bounce-out .5s;
-		}
-		@keyframes bounce-in {
-		  0% {
-		    transform: scale(0);
-		  }
-		  50% {
-		    transform: scale(1.5);
-		  }
-		  100% {
-		    transform: scale(1);
-		  }
-		}
-		@keyframes bounce-out {
-		  0% {
-		    transform: scale(1);
-		  }
-		  50% {
-		    transform: scale(1.5);
-		  }
-		  100% {
-		    transform: scale(0);
-		  }
-		}
+        	#box{-webkit-animation-delay: 2s;
+        	-moz-animation-delay: 2s;
+        	-o-animation-delay: 2s;
+        	animation-delay: 2s;}
         
 		</style>
 	</head>
 	<body>
-        <?php
-
-             include "../../lanewechat.php";
-             $redirect_uri = 'LaneWeChat/yangjiguanjia/keeper/login.php';
-             \LaneWeChat\Core\WeChatOAuth::getCode($redirect_uri, $state=1, $scope='snsapi_base');
-             $code = $_GET['code'];
-             $a = \LaneWeChat\Core\WeChatOAuth::getAccessTokenAndOpenId($code);
-            // var_dump($a);
-
-        ?>
+        
 		<div id="app">
-			<transition v-bind:name="animation_name">
-				<div v-if="show">
-					<div class="container" id="container">
-				        <div class="hongbao">
-				            <div class="topcontent">
-				                <div class="avatar">
-				                    <!--<img src="http://placehold.it/80x80" alt="" width="80" height="80">-->
-				                    
-				                </div>
-				                <h2 style="font-size: 26px;color: white;">老王</h2>
-				                <span class="text" style="font-size: 22px;">给你发了一个红包</span>
-				                <div class="description" style="color:white;font-weight: bold;font-size: 20px;">恭喜发财 大吉大利</div>
-				            </div>
-				            <div id="chai" v-bind:class="{ rotate: isOpen }" @click="open">
-				            	開
-				                <!--<span style="font-size: 26px;font-weight: 600;"></span>-->
-				            </div>
-				        </div>
-				    </div>
+			<div v-if="active">
+				<div id="box" v-bind:class="{  animated: active,fadeOut: active }">
+					<p style="font-size: 30px;color: dodgerblue;text-align: center;margin-top: 40%;font-weight: 600;" id="got" v-bind:class="{ animated: active, 'bounceInRight': active }">手速可以啊,小伙子</p>
 				</div>
-			</transition>
-			<p style="text-align: center;font-size: .32rem;margin-top: .2rem;">这是张宇给您送的蛋蛋哦!</p>
-			<p style="font-size: .36rem;margin: .2rem 0;">新年快乐啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊！</p>
-			<div class="weui-cells weui-cells_form">
-	            <div class="weui-cell">
-	                <div class="weui-cell__hd"><label class="weui-label">配送地址</label></div>
-	                <div class="weui-cell__bd">
-	                    <input class="weui-input" type="text" placeholder="请输入配送地址" v-model="address">
-	                </div>
-	            </div>
-	            <div class="weui-cell weui-cell_vcode">
-	                <div class="weui-cell__hd">
-	                    <label class="weui-label">手机号</label>
-	                </div>
-	                <div class="weui-cell__bd">
-	                    <input class="weui-input" type="tel" placeholder="请输入手机号" v-model="tel">
-	                </div>
-	                <div class="weui-cell__ft">
-	                    <button class="weui-vcode-btn">获取验证码</button>
-	                </div>
-	            </div>
-	            <div class="weui-cell">
-	                <div class="weui-cell__hd"><label class="weui-label">验证码</label></div>
-	                <div class="weui-cell__bd">
-	                    <input class="weui-input" type="number" placeholder="请输入手机验证码" v-model="code">
-	                </div>
-	            </div>
-	            <div class="weui-cell">
-	                <div class="weui-cell__hd"><label class="weui-label">姓名</label></div>
-	                <div class="weui-cell__bd">
-	                    <input class="weui-input" type="text" placeholder="请输入您的姓名" v-model="name">
-	                </div>
-	            </div>
-	        </div>
-	        <div class="button-sp-area" style="width: 80%;margin: 1rem auto .2rem;">
-	            <a href="javascript:void(0);" class="weui-btn weui-btn_plain-primary" @click="submit">提交</a>
-	        </div>	
+			</div>
+			<div v-if="condition">
+				
+					<p style="font-size: 30px;color: dodgerblue;text-align: center;margin-top: 40%;font-weight: 600;" id="err" v-bind:class="{ animated: condition, 'bounceInRight': condition }">{{message}}</p>
+				
+			</div>
+			<div v-if="show">
+				<div class="container" id="container">
+			        <div v-bind:class="{ animated: active, 'fadeOutUp': active }"  id="hongbao">
+			            <div class="topcontent">
+			                <div class="avatar">
+			                    <!--<img src="http://placehold.it/80x80" alt="" width="80" height="80">-->
+			                    
+			                </div>
+			                <h2 style="font-size: 26px;color: white;">老王</h2>
+			                <span class="text" style="font-size: 22px;">给你发了一个红包</span>
+			                <div class="description" style="color:white;font-weight: bold;font-size: 20px;">恭喜发财 大吉大利</div>
+			            </div>
+			            <div id="chai" v-bind:class="{ rotate: isOpen }" @click="open">
+			            	開
+			                <!--<span style="font-size: 26px;font-weight: 600;"></span>-->
+			            </div>
+			        </div>
+			    </div>
+			</div>
+			
 	    </div>
 		<script src="assets/js/vue.min.js" type="text/javascript" charset="utf-8"></script>
 		<script src="assets/js/vue-resource.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="assets/js/commom.js" type="text/javascript" charset="utf-8"></script>
 		<script type="text/javascript">
 			var app = new Vue({
 			  el: '#app',
@@ -226,36 +208,40 @@
 			 	tel:"",
 			 	code:"",
 			 	isOpen:false,
-			 	show:true,
-			 	animation_name:""
-			 	
+			 	show:false,
+			 	active:false,
+			 	show_got:false,
+			 	condition:false,
+			 	message:""
 			  },
 			  created:function(){
-//              var url="json.jsp";
-//				this.$http.get(url).then(function(data){
-//                  //var json=data.body;
-//                  //this.data=eval("(" + json +")");
-//              },function(response){
-//                  //console.info(response);
-//              })
-	         },
-	         methods:{
-	         	submit:function(){
-	         		this.$http.post("test.php",{name:this.name,address:this.address,tel:this.tel,code:this.code},{emulateJSON:true}).then(
+			  		var _this=this
+					this.$http.post(validate.url+"/Api/WxHappyEgg/addCatchLog",{order_sn:"2017010514061787472",open_id:111111},{emulateJSON:true}).then(
 			            function (res) {
 			                // 处理成功的结果
-			                alert(res.body);
+			                if(res.body.code==1){
+			                	_this.show=true;
+			                }else if(res.body.code==0){
+			                	_this.message=res.body.msg;
+			                	_this.condition=true;
+			                }
+			               
 			            },function (res) {
 			            	// 处理失败的结果
+			            	alert("请求失败")
 			            }
 			        )
-	         	},
+	         },
+	         methods:{
+	         	
 	         	open:function(){
-	         		var vm=this
+	         		//var vm=this
 	         		this.isOpen=true;
+	         		this.active=true;
 	         		setTimeout(function(){
-	         			vm.animation_name="bounce";
-	         		},600)
+	         			alert("快去填写收货地址吧！")
+	         			location.href="write_info.html"
+	         		},2500)
 	         	}
 	         }
 			})
