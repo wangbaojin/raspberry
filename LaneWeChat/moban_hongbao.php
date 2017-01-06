73a5121af9214ec0217505a3911d49b7e63c0202
wangbaojin <904026435@qq.com>
2017-01-06 17:31:02 +0800

    日常提交

--- LaneWeChat/moban_hongbao.php
+++ LaneWeChat/moban_hongbao.php
@@ -5,7 +5,7 @@
              'first'=>array('value'=>'您好，您送出的礼品成功领取', 'color'=>'#0A0A0A'),
              'keyword1'=>array('value'=>'点击详情可以查看哦', 'color'=>'#173177'),
              'keyword2'=>array('value'=>'1份快乐的蛋', 'color'=>'#173177'),
-             'keyword3'=>array('value'=>'2017年1月7号', 'color'=>'#173177'),
+             'keyword3'=>array('value'=>date("Y/m/d H:i:s"), 'color'=>'#173177'),
              'remark'=>array('value'=>'请及时确认，15分钟内可以点击详情收回礼品哦', 'color'=>'#173177')
         );
 
@@ -15,6 +15,6 @@
         //$topcolor 顶部颜色，可以为空。默认是红色
         $touser = $_GET['open_id'];
         $templateId = "S7cNdtiV2EfwhQK_UPisTlNESry0Qp9JVlFQ9hV66SI";
-        $url = "http://www.yangjiguanjia.com";
+        $url = "http://weixin.yangjiguanjia.com/LaneWeChat/yangjiguanjia/send_happyegg/share_egg.html?order_sn=".$_GET['order_sn'];
         \LaneWeChat\Core\TemplateMessage::sendTemplateMessage($data, $touser, $templateId, $url, $topcolor='#FF0000');
 
