9e99d56ba609403990c9c8e4a48cb45925dfcedb
wangbaojin <904026435@qq.com>
2017-01-18 16:40:33 +0800

    日常提交

--- LaneWeChat/yangjiguanjia/send_happyegg/assets/js/commom.js
+++ LaneWeChat/yangjiguanjia/send_happyegg/assets/js/commom.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const validate = {
 			//域名
 			url:"http://weixin.yangjiguanjia.com",
@@ -9,16 +8,5 @@ const validate = {
 		        }
 		        return true;
 		    }
-=======
-const validate = {
-			//域名
-			url:"http://weixin.yangjiguanjia.com",
-		    //phone
-		    phone: function(phone){
-		        if(!/^(13[0-9]|15[0|1|2|3|6|7|8|9]|18[0-9]|17[0-9])\d{8}$/.test(phone)){
-		            return false;
-		        }
-		        return true;
-		    }
->>>>>>> develop
+
 }
