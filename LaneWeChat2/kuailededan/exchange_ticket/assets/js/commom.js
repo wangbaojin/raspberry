7c7198106d63fc93e912fe063f814c65b0a66509
Star_sider <dxsider@163.com>
2017-03-08 16:28:43 +0800

    加功能：卡劵兑换

---
+++ LaneWeChat2/kuailededan/exchange_ticket/assets/js/commom.js
@@ -0,0 +1,12 @@
+const validate = {
+			//域名
+			url:"http://weixin.yangjiguanjia.com",
+		    //phone
+		    phone: function(phone){
+		        if(!/^(13[0-9]|15[0|1|2|3|6|7|8|9]|18[0-9]|17[0-9])\d{8}$/.test(phone)){
+		            return false;
+		        }
+		        return true;
+		    }
+
+}
