be461f6e1692fa4e98f8fc8069c3a4aa42ffbf1d
yhw <luckyhw1993@163.com>
2017-04-14 11:12:12 +0800

    日常提交

--- LaneWeChat2/kuailededan/exchange_ticket/login.php
+++ LaneWeChat2/kuailededan/exchange_ticket/login.php
@@ -1,11 +1,12 @@
 <?php
 
    include "../../lanewechat.php"; 
-   $b = $_GET['num']; 
+   $b = $_GET['num'];
    $redirect_uri = 'LaneWeChat2/kuailededan/exchange_ticket/login.php';
-   \LaneWeChat\Core\WeChatOAuth::getCode($redirect_uri, $b, $scope='snsapi_base');
+   \LaneWeChat\Core\WeChatOAuth::getCode($redirect_uri, $b, $scope='snsapi_userinfo');
    $code = $_GET['code'];
    $a = \LaneWeChat\Core\WeChatOAuth::getAccessTokenAndOpenId($code);
+   $user_info = \LaneWeChat\Core\WeChatOAuth::getUserInfo($a['access_token'],$a['openid'],$lang='zh_CN'); 
    $order_sn = $_GET['state'];
      
 ?>
@@ -22,36 +23,46 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/reset.css"/>
 		<link rel="stylesheet" type="text/css" href="https://res.wx.qq.com/open/libs/weui/1.1.0/weui.min.css"/>
 		<style type="text/css">
-			
+			body{background-color: #e5eef2;}
+			.banner{width: 100%;height: auto;}
+			.common_form{height: 0.8rem;padding: 0.1rem 0.2rem;}
+			.icon{height: 0.48rem;}
+			.mr{margin-right: 0.18rem;}
+			.weui-cell:before{border-top: 1px solid #e5e5e5;}
+			.weui-cells{font-size: 0.34rem;}
+			.fontcolor{color:#353535;}
 		</style>
 	</head>
 	<body>
 	
 		<div id="app">
-			<div class="weui-cells__title" style="margin-top: 30px;">快乐的蛋卡劵兑换</div>
-			<div class="weui-cells weui-cells_form">
-	            <div class="weui-cell" id="">
-	                <div class="weui-cell__hd"><label class="weui-label">卡号</label></div>
+			<img class="banner" src="assets/img/ex_banner.png" alt="">
+			<div style="margin-top: 0.4rem;" class="weui-cells weui-cells_form">
+	            <div class="weui-cell common_form" id="">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/card.png" alt=""></div>
 	                <div class="weui-cell__bd">
-	                    <span class="num">{{num}}</span>
+	                    <!-- <span class="num">{{num}}</span> -->
+	                    <input class="weui-input fontcolor" v-model="num" type="number" placeholder="请输入卡号">
 	                </div>
 	            </div>
-	            <div class="weui-cell" id="">
-	                <div class="weui-cell__hd"><label class="weui-label">密码</label></div>
+	            <div class="weui-cell common_form" id="">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/code.png" alt=""></div>
 	                <div class="weui-cell__bd">
-	                    <input class="weui-input" v-model="password" type="number" pattern="[0-9]*" placeholder="请输入兑换码">
+	                    <input class="weui-input fontcolor" v-model="password" type="number" pattern="[0-9]*" placeholder="请输入卡券密码">
+	                </div>
+	            </div>
+	            <div class="weui-cell common_form" id="">
+		            <div class="weui-cell__hd mr"><img class="icon" src="assets/img/number.png" alt=""></div>
+	                <div class="weui-cell__bd">
+	                    <input class="weui-input fontcolor" v-model="yzcode" type="tel" placeholder="请输入验证码">
+	                    <img @click="change_img" style="height:0.9rem;position: absolute;top: 0;right: 0;" v-bind:src="yz_img">
 	                </div>
 	            </div>
 	        </div>
 	        <div class="button-sp-area">
-	        		<a style="width: 70%;margin-top: 30px;" href="javascript:void(0);" id="create" class="weui-btn weui-btn_plain-primary" @click="exchange">兑换</a>
+	        		<img @click="exchange" style="width: 6.9rem;margin:0.4rem auto 0.2rem;" src="assets/img/button.png" alt="">
+	        		<p style="font-size: 0.22rem;color: #888;margin-left: 0.3rem;">*输入卡券仅代表验证，提交订单才会致卡券被使用</p>
 			</div>
-			<div class="weui-footer weui-footer_fixed-bottom">
-	            <p class="weui-footer__links">
-	                <a href="http://www.yangjiguanjia.com" class="weui-footer__link">邦铭农信科技(北京)有限公司</a>
-	            </p>
-	            <p class="weui-footer__text">Copyright &copy; 2016-2017  </p>
-	        </div>
 	    </div>
 		<script src="assets/js/vue.min.js" type="text/javascript" charset="utf-8"></script>
 		<script src="assets/js/vue-resource.min.js" type="text/javascript" charset="utf-8"></script>
@@ -61,7 +72,10 @@
 			  el: '#app',
 			  data: {
 			     num:"您的卡劵卡号",
-			     password:""
+			     password:"",
+			     yzcode:"",
+			     yz_img:"http://weixin.yangjiguanjia.com/deliveryLogGetVerify",
+			     card_tp:""
 			  },
 			  methods:{
 			  	
@@ -73,27 +87,41 @@
 			  		}else if(_this.password==""){
 			  			alert("请输入密码")
 			  			return
+			  		}else if(_this.yzcode==""){
+			  			alert("请输入验证码")
+			  			return
 			  		}
-			  		this.$http.post(validate.url+"/Api/DeliveryLog/checkCard",{card_no:_this.num,card_pw:_this.password,open_id:"<?php echo $a['openid']; ?>"},{emulateJSON:true}).then(
-			            function (res) {
-			                if(res.body.code==1){
-			                		localStorage.setItem("exchange_info",JSON.stringify({"order_sn":_this.num,"password":_this.password,open_id:"<?php echo $a['openid']; ?>"}))
-			                		alert(res.body.msg);
-			                		location.href="write_info.php"
-			                }else{
-			                		alert(res.body.msg)
-			                }
+			  		 this.$http.post(validate.url+"/deliveryLogCheckCard",{card_no:_this.num,card_pw:_this.password,open_id:"<?php echo $a['openid']; ?>"},{emulateJSON:true}).then(
+			              function (res) {
+			                 if(res.body.code==1){
+			                 		_this.card_tp=res.body.result.card_tp;
+									_this.$http.post(validate.url+"/deliveryLogCheck_verify",{code:_this.yzcode},{emulateJSON:true}).then(
+											function(res){
+												if(res.body.code==1){
+													localStorage.setItem("exchange_info",JSON.stringify({"order_sn":_this.num,"card_tp":_this.card_tp,"nick_name":"<?php echo $user_info['nickname'];?>","pic":"<?php echo $user_info['headimgurl']; ?>"}))
+			                  							location.href="write_info.php"
+												}else{
+													alert("验证码错误")
+												}
+											}
+										)
+			                  }else{
+			                  		alert(res.body.error)
+			                  }
 			                
-			            },function (res){
-			            		// 处理失败的结果
-			           	 	alert("请求失败!")
-			            }
+			              },function (res){
+			              		// 处理失败的结果
+			             	 	alert("请求失败!")
+			              }
 			        )
+			  	},
+			  	change_img:function(){
+			  		this.yz_img="http://weixin.yangjiguanjia.com/deliveryLogGetVerify"+Math.random();
 			  	}
 			  },
-			  created:function(){
-			  	this.num="<?php echo $order_sn;?>"
-			  }
+			   created:function(){
+			   	this.num="<?php echo $order_sn;?>"
+			   }
 			})
 		</script>
 	</body>
--- LaneWeChat2/kuailededan/exchange_ticket/write_info.php
+++ LaneWeChat2/kuailededan/exchange_ticket/write_info.php
@@ -2,7 +2,7 @@
 
    include "../../lanewechat.php";
    $redirect_uri = 'LaneWeChat2/kuailededan/exchange_ticket/write_info.php';
-   \LaneWeChat\Core\WeChatOAuth::getCode($redirect_uri, $state=1, $scope='snsapi_base');
+    \LaneWeChat\Core\WeChatOAuth::getCode($redirect_uri, $state=1, $scope='snsapi_base');
    $code = $_GET['code'];
    $a = \LaneWeChat\Core\WeChatOAuth::getAccessTokenAndOpenId($code);
 ?>
@@ -17,84 +17,171 @@
 		<title>快乐的蛋－兑换</title>
 		<script src="assets/js/rem.js" type="text/javascript" charset="utf-8"></script>
 		<link rel="stylesheet" type="text/css" href="assets/css/reset.css"/>
+		<link rel="stylesheet" type="text/css" href="assets/css/normalize.css"/>
+		<link rel="stylesheet" type="text/css" href="assets/css/stylesheet.css"/>
 		<link rel="stylesheet" type="text/css" href="https://res.wx.qq.com/open/libs/weui/1.1.0/weui.min.css"/>
 		<style type="text/css">
 			.weui-picker{	font-size: 15px;}
+			.banner{width: 100%;height: auto;}
+			.common_form{height: 0.8rem;padding: 0.1rem 0.2rem;}
+			.icon{height: 0.48rem;margin-right: 0.16rem;}
+			.weui-cell:before {content: " ";position: absolute;left: 0;top: 0;right: 0;height: 1px;border-top: 1px solid #e5e5e5;color: #e5e5e5;-webkit-transform-origin: 0 0;transform-origin: 0 0;-webkit-transform: scaleY(.5);transform: scaleY(.5);left: 0.76rem;}
+			.weui-cell__hd{font-size: 0.34rem;color:#353535;}
+			.sex{width: 1rem;display: inline-block;margin-right: 1rem;margin-left: 0.16rem;color: #353535;}
+			.weui-cells{font-size: 0.34rem;}
+			.weui-icon-warn:before{content: "\EA0B";}
+			.weui-cell_warn .weui-icon-warn {display: inline-block;}
+			.fontcolor{color:#353535;}
+			.date{width: 93%;display: inline-block;}
+			/*.weui-icon-warn{font-size: 0.18rem;}*/
+			.notice{height: 0.5rem;line-height: 0.5rem;background-color: #000;opacity: 0.5;width: 3rem;border-radius: 0.3rem;position: absolute;top: -0.8rem;left: 50%;margin-left: -1.5rem;font-size: 0.22rem;color:#fff;text-align: center;}
+			.weui-picker__indicator {
+    				height: 31px;
+    				top: 107px;	
+			}
+			.weui-picker{font-size:18px;}
+			.weui-picker__group{display: none;}
 		</style>
 	</head>
 	<body>
 	
 		<div id="app">
-			<div class="weui-cells__title" style="margin-top: 30px;">快乐的蛋信息填写</div>
-			<div class="weui-cells weui-cells_form">
-	            <div class="weui-cell" id="">
-	                <div class="weui-cell__hd"><label class="weui-label">姓名</label></div>
+			<img class="banner" src="assets/img/ex_banner.png" alt="">
+			<div style="margin-top: 0.4rem;" class="weui-cells weui-cells_form">
+	            <div class="weui-cell common_form" id="">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/name.png" alt=""></div>
 	                <div class="weui-cell__bd">
-	                    <input class="weui-input" v-model="name" type="text" placeholder="请填写您的姓名">
+	                    <input class="weui-input fontcolor" v-model="name" type="text" placeholder="请填写您的姓名">
 	                </div>
 	            </div>
-	            <div class="weui-cell" id="">
-	                <div class="weui-cell__hd"><label class="weui-label">手机</label></div>
-	                <div class="weui-cell__bd">
-	                    <input class="weui-input" v-model="tel" type="tel"  placeholder="请输入手机号码">
+	            <div style="padding-left: 0.76rem;" class="weui-cell common_form" id="">
+	                <div @click="choose_sex(0)" class="weui-cell__hd mr">
+	                	<img style="display: inline-block;height: 0.4rem;" v-bind:src="tick" alt="">
+	                	<label class="weui-label sex">先生</label>
+	                </div>
+	                <div @click="choose_sex(1)" class="weui-cell__hd mr">
+	                	<img style="display: inline-block;height: 0.4rem;" v-bind:src="square" alt="">
+	                	<label class="weui-label sex">女士</label>
 	                </div>
 	            </div>
-	            <div class="weui-cell" id="">
-	                <div class="weui-cell__hd"><label class="weui-label">地址</label></div>
+	            <div class="weui-cell common_form" id="">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/tel.png" alt=""></div>
+	                <div @click="icon_dis" class="weui-cell__bd">
+	                    <input style="width: 90%;" class="weui-input fontcolor" v-model="tel" type="tel"  placeholder="请输入手机号码">
+	                </div>
+	                <div class="weui-cell__ft">
+                    		<i v-bind:style="{display:icon}" class="weui-icon-warn"></i>
+                	</div>
+	            </div>
+				<div id="picker5" class="weui-cell common_form" id="" @click="choose_add">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/location.png" alt=""></div>
 	                <div class="weui-cell__bd">
-	                    <input class="weui-input" v-model="address" type="text" placeholder="请填写您的收货地址">
+	                    <span v-bind:style="{color:font_color}" class="date">{{address}}</span>
+	                    <!-- <input class="weui-input fontcolor" v-model="address" type="text" placeholder="请填写地址"> -->
+	                    <img style="height: 0.36rem;display: inline-block;margin-top: 0.05rem;" src="assets/img/arrow.png" alt="">
 	                </div>
 	            </div>
-	            
-	            <div class="weui-cell" id="datePicker" @click="choose_date">
-	                <div class="weui-cell__hd"><label class="weui-label">日期</label></div>
+	            <div class="weui-cell common_form" id="">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/address.png" alt=""></div>
 	                <div class="weui-cell__bd">
-	                    <span class="date" style="color: lightgray;">{{date}}</span>
+	                    <input class="weui-input fontcolor" v-model="address_details" type="text" placeholder="请精确填写至门牌号">
 	                </div>
 	            </div>
+	            <div class="weui-cell common_form" id="datePicker" @click="choose_date">
+	                <div class="weui-cell__hd mr"><img class="icon" src="assets/img/address.png" alt=""></div>
+	                <div class="weui-cell__bd">
+	                    <span v-bind:style="{color:fontcolor}" class="date">{{date}}</span>
+	                    <img style="height: 0.36rem;display: inline-block;margin-top: 0.05rem;" src="assets/img/arrow.png" alt="">
+	                </div>
+	            </div>
+
 	        </div>
-	        <div class="button-sp-area">
-	        		<a style="width: 70%;margin-top: 30px;" href="javascript:void(0);" id="create" class="weui-btn weui-btn_plain-primary" @click="sure">确定</a>
+	        <p style="font-size: 0.22rem;color: #888;margin-right: 0.3rem;text-align: right;margin-top: 0.2rem;">*每周日配送，共计{{send_num}}次</p>
+	        <div style="position: relative;" class="button-sp-area">
+	        		 <p v-show="notice" class="notice">*请正确填写电话号码</p>
+	        		<img @click="sure" style="width: 6.9rem;margin:0.4rem auto 0.2rem;" src="assets/img/button_2.png" alt="">
 			</div>
-			<div class="weui-footer weui-footer_fixed-bottom">
-	            <p class="weui-footer__links">
-	                <a href="tel:010-84599723" class="weui-footer__link" style="font-size: 18px;">010-84599723</a>
-	            </p>
-	            <p class="weui-footer__text">邦铭农信科技(北京)有限公司  </p>
-	        </div>
 	    </div>
 		<script src="assets/js/vue.min.js" type="text/javascript" charset="utf-8"></script>
 		<script src="assets/js/vue-resource.min.js" type="text/javascript" charset="utf-8"></script>
 		<script src="assets/js/weui.min.js" type="text/javascript" charset="utf-8"></script>
+		<script src="assets/js/picker.min.js" type="text/javascript" charset="utf-8"></script>
 		<script src="assets/js/commom.js" type="text/javascript" charset="utf-8"></script>
 		<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+		<script src="assets/js/city.js" type="text/javascript" charset="utf-8"></script>
+		<script src="assets/js/index.js" type="text/javascript" charset="utf-8"></script>
 		<script type="text/javascript">
+			history.pushState({page : 'state1'},'state','#state1');
+			history.pushState({page : 'state2'},'state','#state2');
+			
+			window.onpopstate = function(event) {
+		      if (event.state.page === 'state1') {
+		        //WeixinJSBridge.call('closeWindow');
+		        WeixinJSBridge.invoke('closeWindow',{},function(res){
+
+				    //alert(res.err_msg);
+				
+				});
+		      }
+		    }
 			var app = new Vue({
 			  el: '#app',
 			  data: {
 			    name:"",
-			 	address:"",
+			 	address:"请填写地址",
+			 	address_details:"",
 			 	tel:"",
 			 	date:"请选择配送日期",
-			 	order_sn:null
+			 	order_sn:null,
+			 	tick:"assets/img/tick.png",
+			 	square:"assets/img/square.png",
+			 	icon:"none",
+			 	fontcolor:"#b2b2b2",
+			 	font_color:"#b2b2b2",
+			 	notice:false,
+			 	card_tp:"",
+			 	send_num:"",
+			 	city:""
 			  },
 			  methods:{
 			  	choose_date:function(){
-			  		var _this=this;
+			  		this.fontcolor="#353535";
+			  		_this=this;
 			  		weui.datePicker({
-					    start: new Date(),
-					    end: "2017-"+_this.judge(new Date().getMonth()+2)+"-1",
-					    //defaultValue: [3],
-					    cron: '* * 6',
-					    onChange: function(result){
-					       _this.date=Number(result[0].value+_this.judge(result[1].value)+_this.judge(result[2].value));   
-					    },
-					    onConfirm: function(result){
-					        _this.date=Number(result[0].value+_this.judge(result[1].value)+_this.judge(result[2].value));
-					        
-					    },
-					    id: 'datePicker'
+            			start: 2017,
+            			end: new Date().getFullYear(),
+	            		onChange: function (result) {
+	                		//console.log(result);
+	                		month=result[1]+1;
+	                		_this.date=result[0]+'-'+_this.judge(month)+'-'+_this.judge(result[2]);
+	            		},
+	            		onConfirm: function (result) {
+	                		//console.log(result);
+	            		}
 					});	
+			  	},
+			  	choose_sex:function(index){
+			  		_this=this;
+			  		if(index==0){
+			  			_this.tick="assets/img/tick.png";
+			  			_this.square="assets/img/square.png";
+			  		}else{
+			  			_this.tick="assets/img/square.png";
+			  			_this.square="assets/img/tick.png";
+			  		}
+			  	},
+			  	choose_add:function(){
+			  		_this=this;
+			  		this.font_color="#353535";
+			  		picker.show();
+			  		picker.on('picker.valuechange', function (selectedVal, selectedIndex) {
+  						console.log(nameEl.innerText)
+  						_this.address=nameEl.innerText;
+					});
+			  	},
+			  	icon_dis:function(){
+			  		this.icon="none";
+			  		this.notice=false;
 			  	},
 			  	sure:function(){
 	         		var _this=this;
@@ -104,19 +191,28 @@
 			  		}else if(this.address==""){
 			  			alert("请输入地址")
 			  			return
+			  		}else if(this.address_details==""){
+			  			alert("请输入门牌号")
+			  			return
 			  		}else if(!validate.phone(_this.tel)){
-			  			alert("请输入正确的手机号")
+			  			_this.icon="block";
+			  			_this.notice=true;
 			  			return
 			  		}else if(this.date=="配送日期"){
 			  			alert("请选择您的配送日期")
 			  			return
 			  		}
-			  		this.$http.post(validate.url+"/Api/DeliveryLog/add",{tel:_this.tel,real_name:_this.name,address:_this.address,open_id:"<?php echo $a['openid']; ?>",order_sn:_this.order_sn},{emulateJSON:true}).then(
+			  		var nick_name=JSON.parse(localStorage.getItem("exchange_info")).nick_name;
+					var pic=JSON.parse(localStorage.getItem("exchange_info")).pic;
+					this.order_sn=JSON.parse(localStorage.getItem("exchange_info")).order_sn;
+			  		this.$http.post(validate.url+"/deliveryLogAdd",{tel:_this.tel,real_name:_this.name,address:_this.address,open_id:"<?php echo $a['openid']; ?>",order_sn:_this.order_sn,delivery_time:_this.date,nick_name:nick_name,pic:pic},{emulateJSON:true}).then(
 			            function (res) {
 			                if(res.body.code==1){
-			                		alert(res.body.msg)
+			                		alert("兑换成功")
+			                		localStorage.setItem("user_info",JSON.stringify({"order_sn":_this.order_sn,"address":_this.address+_this.address_details,"date":_this.date,"tel":_this.tel,"name":_this.name,"send_num":_this.send_num}))
+			                		location.href="success.html"
 			                }else{
-			                		alert(res.body.msg)
+			                		alert(res.body.error)
 			                }
 			                
 			            },function (res){
@@ -135,7 +231,16 @@
 			  },
 			  created:function(){
 			  	var _this=this;
-				this.order_sn=JSON.parse(localStorage.getItem("exchange_info")).order_sn;
+				this.card_tp=JSON.parse(localStorage.getItem("exchange_info")).card_tp;
+				if(_this.card_tp==3){
+					_this.send_num=2;
+				}else if(_this.card_tp==1){
+					_this.send_num=4;
+				}else if(_this.card_tp==2){
+					_this.send_num=10;
+				}else if(_this.card_tp==4){
+					_this.send_num=20;
+				}
 			    var url=location.href.split("&")[0]+"%26"+location.href.split("&")[1];
 		  		_this.$http.get(validate.url+'/LaneWeChat/api_getsign.php?url='+url).then(function(res){
          			//alert(res.body)
@@ -153,8 +258,8 @@
 						    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 						    success: function (res) {
 						    	
-						        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-						        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+						        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+						        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 						  		
 						  		_this.$http.get(validate.url+'/getAddressInfo/geocoder/v2/?location='+latitude+','+longitude+'&output=json&pois=1&ak=0r5h4bhrVoUjEvrNc8Lx0NLUcP9xiaQo&coordtype=wgs84ll').then(function(res){
 				                    
@@ -163,6 +268,11 @@
 				                    if(res.status==0){
 				                    	
 				                    		_this.address=res.result.formatted_address;
+				                    		//_this.city=_this.address.substr(0,2)
+				                    		// if(_this.city=="黑龙江"){
+				                    		// 	_this.city="黑龙江"
+				                    		// }
+				                    		
 				                    }else{
 				                    		alert("定位失败！")
 				                    }
